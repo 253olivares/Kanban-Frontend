11ba01a0aead45d05a6c5291944a9101943ab681
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-04-09 01:49:20 -0400

    adjustments

--- src/modals/index.tsx
+++ src/modals/index.tsx
@@ -1,11 +1,14 @@
 import { useLockBodyScroll } from "@uidotdev/usehooks";
 import { useAppDispatch } from "../reduxStore/hook";
 import { closeModal } from "./modalSlice";
+import { useAppSelector } from "../reduxStore/hook";
+import { getModalType } from "./modalSlice";
 
 // this is our modal container that will show and hide modals based on what is suppose to be showing
 const index = () => {
-
   const dispatch = useAppDispatch();
+  const modal = useAppSelector(getModalType);
+  console.log(modal);
     // react hook by ui.dev that will over write the body overflow property
     // The hook as a use effect that overwrites our property and save its previous value then when the component unmounts it 
     // sets the overflow back to its original value
