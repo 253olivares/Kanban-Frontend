5f2a5e69acf6cc2df1ce222c8c760cf54102b102
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-04-09 20:57:29 -0400

    loading additions and added lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from './pages/1_HomePage';
-import LayoutAccount from './pages/components/AccountHeader';
-import AccountLanding from './pages/2_AccountLanding';
-import Workspace from './pages/3_Workspace';
+import { lazy } from "react";
+import LoadingSpinner from './pages/components/LoadingSpinner';
 import Errorpage from './pages/0_404Page';
+
+const HomePage = lazy(()=> import('./pages/1_HomePage'));
+const AccountLanding = lazy(()=> import('./pages/2_AccountLanding'));
+const LayoutAccount = lazy(()=> import('./pages/components/AccountHeader'));
+const Workspace = lazy(()=> import('./pages/3_Workspace'));
 
 
 // out app component to gather all our components
 function App(): JSX.Element {
-
 // return our page
   return (
     <React.Fragment>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        {/* our heading layout */}
-        
-        <Route path="u/:id" element={<LayoutAccount />} >
-          {/* account landing page where users will arrive when they login */}
-          <Route index element={<AccountLanding/>} />
-          {/* Workspace Page that is where the user can view all ongoing tasks */}
-          <Route path="workspace/:id" element={<Workspace />}/>
-        </Route>
+      <React.Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+            <Route path="/" element={<HomePage />}/>
+            {/* our heading layout */}
 
-        {/* 404 page */}
-        <Route path="*" element={<Errorpage />} />
-      </Routes>
+            <Route path="u/:id" element={<LayoutAccount />} >
+              {/* account landing page where users will arrive when they login */}
+              <Route index element={<AccountLanding/>} />
+              {/* Workspace Page that is where the user can view all ongoing tasks */}
+              <Route path="workspace/:id" element={<Workspace />}/>
+            </Route>
+
+            {/* 404 page */}
+            <Route path="*" element={<Errorpage />} />
+        </Routes>
+      </React.Suspense>
     </React.Fragment>
   )
 }
---
+++ src/pages/components/LoadingSpinner/index.tsx
@@ -0,0 +1,18 @@
+import logo from '/assets/Logo_Export.svg'
+
+const index = () => {
+    // animate-[spin_8s_linear_infinite] 
+  return (
+    <main className='w-screen h-screen bg-PrimaryWhite relative flex flex-col gap-14 justify-center items-center scale-75 sLaptop:scale-100'>
+        <img src={logo} alt="Logo" />
+        <div className='flex gap-4'>
+            <div className='w-[3rem] h-[3rem] bg-black opacity-50 block rounded-full animate-[reversebounce_1s_infinite] animationDelay3s' />
+            <div className='w-[3rem] h-[3rem] bg-black opacity-50 block rounded-full animate-[reversebounce_1s_infinite] animationDelay2s' />
+            <div className='w-[3rem] h-[3rem] bg-black opacity-50 block rounded-full animate-[reversebounce_1s_infinite] animationDelay1s' />
+            <div className='w-[3rem] h-[3rem] bg-black opacity-50 block rounded-full animate-[reversebounce_1s_infinite]' />
+        </div>
+    </main>
+  )
+}
+
+export default index
