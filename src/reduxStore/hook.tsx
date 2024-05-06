b971d9fbe0b361d6787d80949b5ad77323976e8e
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-05-06 14:29:14 -0400

    Remove all files to fix directory bug

--- src/reduxStore/hook.tsx
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector } from "react-redux"
-import { TypedUseSelectorHook } from "react-redux"
-import { AppDispatch, RootState } from "./store"
-
-
-// create custom hooks that we can use to access our state
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
